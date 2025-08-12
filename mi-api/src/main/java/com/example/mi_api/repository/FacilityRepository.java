e2284b0833ca554a2fb7ddb9385aca0c794f6ac8
rafaelH05 <rafa2005lara@gmail.com>
2025-08-12 19:42:50 +0200

    Delete mi-api directory

--- mi-api/src/main/java/com/example/mi_api/repository/FacilityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mi_api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.mi_api.entities.Facility;
-
-public interface FacilityRepository extends JpaRepository<Facility, Integer> {
-}
--- mi-api/src/main/java/com/example/mi_api/repository/TokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mi_api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.mi_api.entities.VerificationToken;
-
-public interface TokenRepository extends JpaRepository<VerificationToken, Integer>{
-    
-}
