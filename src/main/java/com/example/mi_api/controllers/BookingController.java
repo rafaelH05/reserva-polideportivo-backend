0c71e538fc60422bb5fea33347eca44045b82cd1
rafaelH05 <rafa2005lara@gmail.com>
2026-02-09 13:36:42 +0100

    arreglo imports

--- src/main/java/com/example/mi_api/controllers/BookingController.java
+++ src/main/java/com/example/mi_api/controllers/BookingController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import java.time.ZonedDateTime;
 import java.time.ZoneId;
-import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatter;
 
 @RestController
 
